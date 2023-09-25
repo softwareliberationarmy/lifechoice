6a3080af9799926f925e2a64a5a6de11e9d07546
Kerry Patrick <softwareliberationarmy@gmail.com>
2023-09-25 13:50:19 -0500

    adding web build steps to end of build script

--- build/build.ps1
+++ build/build.ps1
@@ -1,6 +1,25 @@
-# build the lifechoice solution one directory up
 
-Write-Host "Creating build script for lifechoice solution"
+# set the current directory to the directory of the script
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+Set-Location $scriptPath
+
+function Write-Status($message) {
+    Write-Host $message -ForegroundColor Green
+}
+
+Write-Status "BUILDING the LifeChoice solution" -ForegroundColor Green
+
+Write-Status "RESTORING SOLUTION PACKAGES..." 
 dotnet restore ../lifechoice.sln
+Write-Status "BUILDING SOLUTION..."
 dotnet build ../lifechoice.sln -c Release --no-restore 
-dotnet test ../lifechoice.sln -c Release --no-restore 
+Write-Status "RUNNING .NET TESTS..."
+dotnet test ../lifechoice.sln -c Release --no-restore
+
+Write-Status "RUNNING WEB TESTS..." 
+cd ..\client-app
+npm install
+npm run build
+npm test run -- --coverage
+
+Set-Location $scriptPath
